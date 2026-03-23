f70536d222336b93a42be23967a1f9a0bc1e6948
angel <26006790@galileo.edu>
2026-03-23 22:32:41 +0000

    prueba html

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
+<!DOCTYPE html>
+
+<html>
+    <head></head>
+    <body>
+        <h1>ANGEL MANUEL GUIERREZ HERNANDEZ</h1>
+        <h2>Experiencia Laboral</h2>
+        TEXTO
+        <h2>Formacion academica</h2>
+        TEXTO
+    </body>
 
 
 
-Angel Manuel Gutierrez Hernndez
-
-Hola
+</html>
